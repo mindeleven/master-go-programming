069b861f1580bd0b2a2642f89ca4408bad5d6953
mindeleven <mindeleven@posteo.de>
2024-02-19 14:21:34 +0100

    Added comments section

--- formatting_excercise/main.go
+++ formatting_excercise/main.go
@@ -1,3 +1,10 @@
+/////////////////////////////////
+// Coding along with the Udemy course
+// Go (Golang) Programming: The Complete Go Bootcamp 2024
+// https://www.udemy.com/course/master-go-programming-complete-golang-bootcamp/
+// Source code and comments take from the videos of this course
+/////////////////////////////////
+
 package main
 
 import "fmt"
